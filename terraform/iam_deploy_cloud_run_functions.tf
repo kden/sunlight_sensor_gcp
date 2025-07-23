a8d88e5fc6ef6a216da142ca0ced27d58bc0d6ea
Caden Howell <caden@codepawconsulting.com>
2025-07-23 00:36:12 -0500

    Update comments regarding permissions for deploying cloud run functions

--- terraform/iam_deploy_cloud_run_functions.tf
+++ terraform/iam_deploy_cloud_run_functions.tf
@@ -13,12 +13,15 @@ resource "google_service_account" "function_deployer" {
   display_name = "GitHub Actions Function Deployer"
 }
 
-# Would rather not use this since we have a custom service account
-# resource "google_service_account_iam_member" "function_deployer_act_as_compute_default" {
-#   service_account_id = "projects/${var.gcp_project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-#   role               = "roles/iam.serviceAccountUser"
-#   member             = "serviceAccount:${google_service_account.function_deployer.email}"
-# }
+
+# It looks like Cloud Build impersonates the default service account during build phase, so it's currently
+# going to cause an error if we revoke this permission.  We can look into creating (yet another) service account
+# to isolate this, but I'm leaving it for now.
+resource "google_service_account_iam_member" "function_deployer_act_as_compute_default" {
+  service_account_id = "projects/${var.gcp_project_id}/serviceAccounts/${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.function_deployer.email}"
+}
 
 # --- Grant the Service Account permission to deploy Cloud Functions ---
 resource "google_project_iam_member" "function_deployer_functions_admin" {
