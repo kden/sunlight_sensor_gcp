7ab24a51290864861d682dba609eb06f43ebd47c
Caden Howell <caden@codepawconsulting.com>
2025-07-25 14:49:13 -0500

    Remove test pubsub subscription from Terraform

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,33 +1,13 @@
 # terraform/pubsub.tf
 
-
-
 # Create the Pub/Sub topic
 resource "google_pubsub_topic" "sun_sensor_ingest" {
   project = var.gcp_project_id
   name    = "sun-sensor-ingest-topic"
 }
 
-# Create a subscription for testing and verification
-resource "google_pubsub_subscription" "sun_sensor_ingest_test_sub" {
-  project = var.gcp_project_id
-  name    = "sun-sensor-ingest-test-sub"
-  topic   = google_pubsub_topic.sun_sensor_ingest.name
-
-  # The message is kept for 1 day if not acknowledged
-  message_retention_duration = "86400s"
-  # After a message is pulled, it must be ack'd within 20 seconds
-  ack_deadline_seconds = 20
-}
-
 # Output the topic name for reference
 output "pubsub_topic_name" {
   value       = google_pubsub_topic.sun_sensor_ingest.name
   description = "The name of the created Pub/Sub topic."
-}
-
-# Output the subscription name for reference
-output "pubsub_subscription_name" {
-  value       = google_pubsub_subscription.sun_sensor_ingest_test_sub.name
-  description = "The name of the created Pub/Sub subscription."
 }
