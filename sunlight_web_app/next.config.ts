7d285a373ae386b4224262ae1f67c88ed978a542
Caden Howell <caden@codepawconsulting.com>
2025-12-07 13:32:48 -0600

    [#10] CVE-2025-55182 React2Shell update

--- sunlight_web_app/next.config.ts
+++ sunlight_web_app/next.config.ts
@@ -16,13 +16,6 @@ const nextConfig: NextConfig = {
   // in a directory named "out", which is what Firebase Hosting expects.
   output: 'export',
 
-  // Add this block to prevent ESLint errors from failing the build.
-  // The build will still report the errors, but it won't stop the deployment.
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   // Your other Next.js config can go here if you have any.
 };
 
